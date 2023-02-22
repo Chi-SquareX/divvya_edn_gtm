72e5a04459b87cac9197f57d4148ca65db482c25
Rahul Golder <65487329+Goldenprince8420@users.noreply.github.com>
2023-02-22 13:36:26 +0530

    Update test_on_images.py

--- test_on_images.py
+++ test_on_images.py
@@ -14,6 +14,60 @@ from test import start_testing, start_testing_final_images
 img_size = 512
 
 
+def calculate_psnr(img1, img2):
+    # img1 and img2 have range [0, 255]
+    img1 = img1.astype(np.float64)
+    img2 = img2.astype(np.float64)
+    mse = np.mean((img1 - img2)**2)
+    if mse == 0:
+        return float('inf')
+    return 20 * math.log10(255.0 / math.sqrt(mse))
+
+
+def ssim(img1, img2):
+    C1 = (0.01 * 255)**2
+    C2 = (0.03 * 255)**2
+
+    img1 = img1.astype(np.float64)
+    img2 = img2.astype(np.float64)
+    kernel = cv2.getGaussianKernel(11, 1.5)
+    window = np.outer(kernel, kernel.transpose())
+
+    mu1 = cv2.filter2D(img1, -1, window)[5:-5, 5:-5]  # valid
+    mu2 = cv2.filter2D(img2, -1, window)[5:-5, 5:-5]
+    mu1_sq = mu1**2
+    mu2_sq = mu2**2
+    mu1_mu2 = mu1 * mu2
+    sigma1_sq = cv2.filter2D(img1**2, -1, window)[5:-5, 5:-5] - mu1_sq
+    sigma2_sq = cv2.filter2D(img2**2, -1, window)[5:-5, 5:-5] - mu2_sq
+    sigma12 = cv2.filter2D(img1 * img2, -1, window)[5:-5, 5:-5] - mu1_mu2
+
+    ssim_map = ((2 * mu1_mu2 + C1) * (2 * sigma12 + C2)) / ((mu1_sq + mu2_sq + C1) *
+                                                            (sigma1_sq + sigma2_sq + C2))
+    return ssim_map.mean()
+
+
+def calculate_ssim(img1, img2):
+    '''calculate SSIM
+    the same outputs as MATLAB's
+    img1, img2: [0, 255]
+    '''
+    if not img1.shape == img2.shape:
+        raise ValueError('Input images must have the same dimensions.')
+    if img1.ndim == 2:
+        return ssim(img1, img2)
+    elif img1.ndim == 3:
+        if img1.shape[2] == 3:
+            ssims = []
+            for i in range(3):
+                ssims.append(ssim(img1, img2))
+            return np.array(ssims).mean()
+        elif img1.shape[2] == 1:
+            return ssim(np.squeeze(img1), np.squeeze(img2))
+    else:
+        raise ValueError('Wrong input image dimensions.')
+        
+        
 def preprocess_image(cv_img):
     cv_img = cv2.resize(cv_img, (img_size,img_size))
     img = np.array(cv_img)
@@ -74,6 +128,7 @@ def run_on_general_data():
 
         img_name = get_file_name(img_path)
         ori_image = cv2.imread(img_path)
+        ori_image_ = ori_image.copy()
         h, w, _ = ori_image.shape
 
         # ori_image_resized = cv2.resize(ori_image, (img_size,img_size))
@@ -85,7 +140,7 @@ def run_on_general_data():
         save_dir = './result/'
         modelDir = './weights/PMS-Net.h5'
         # print(img_name)
-        start_testing(base_path_hazyImg, base_path_result, img_name, save_dir, modelDir)
+        start_testing(base_path_hazyImg, base_path_result, img_name, save_dir, modelDir)        
         out_path = save_dir + 'py_recover_' + str(img_name.split('.')[0]) + '.jpg'
         t = cv2.imread(out_path)
         t = cv2.cvtColor(t, cv2.COLOR_BGR2GRAY)
@@ -108,6 +163,8 @@ def run_on_general_data():
         de_test = cv2.resize(de_test, (w, h))
 
         rgb_de_test = cv2.cvtColor(de_test, cv2.COLOR_BGR2RGB)
+        print("PSNR value: {}".format(calculate_psnr(ori_image_, rgb_de_test)))
+        print("SSIM value: {}".format(calculate_ssim(ori_image_, rgb_de_test)))
         cv2.imwrite(f"{output_dir}/{img_name}.jpg", rgb_de_test)
 
         cnt+=1
@@ -124,6 +181,7 @@ def run_on_test_data():
 
         img_name = get_file_name(img_path)
         ori_image = cv2.imread(img_path)
+        ori_image_ = ori_image.copy()
         h, w, _ = ori_image.shape
 
         # ori_image_resized = cv2.resize(ori_image, (img_size,img_size))
@@ -158,6 +216,8 @@ def run_on_test_data():
         de_test = cv2.resize(de_test, (w, h))
 
         rgb_de_test = cv2.cvtColor(de_test, cv2.COLOR_BGR2RGB)
+        print("PSNR value: {}".format(calculate_psnr(ori_image_, rgb_de_test)))
+        print("SSIM value: {}".format(calculate_ssim(ori_image_, rgb_de_test)))
         cv2.imwrite(f"{output_dir}/{img_name}.jpg", rgb_de_test)
 
         cnt+=1
